6a9b51f13be29e3e76384997942959d2d210f855
somefe <somefe@gmail.com>
2026-03-04 13:16:44 +0800

    fix

--- .next/dev/static/chunks/src_app_(dashboard)_layout_tsx_6069fb9d._.js
+++ .next/dev/static/chunks/src_app_(dashboard)_layout_tsx_6069fb9d._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_b12ccdb6._.js",
+  "static/chunks/node_modules_dd87cf1f._.js",
   "static/chunks/src_ceb1b685._.js"
 ],
     source: "dynamic"
